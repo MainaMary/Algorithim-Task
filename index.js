fc5bdce8638154736081908ab64298369e851e14
WanjikuMaina <52330368+MaryMaina98@users.noreply.github.com>
2021-05-20 15:10:57 -0700

    Update index.js
    typeof operator

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
   
+    
   function convertFahrToCelsius(number){
-   console.log(`${number}`)
-   console.log('hey')
+      if(typeof number == 'number'){
+        
+        let c = (number - 32) * 5/9;
+        let output = c.toFixed(4)
+        console.log(output)
+      } else if (typeof number == 'string'){
+        let text ='deg c'
+        let convert = parseInt(number, 10)
+        let value = (convert - 32) * 5/9;
+
+        console.log(`${value.toFixed(4)} ${text}`)
+
+      }else if(typeof number == 'array'){
+          
+          console.log('array')
+          console.log(`${number} not a valid number but an array`)
+      }else{
+          console.log(`${number} not a valid number but an object`)
+      }
+  
   }
-  const number = 8;
+  const number = [1,2,3]
   convertFahrToCelsius(number)
